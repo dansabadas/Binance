b17f104c43587a93da857e1c8db6d91fab61625b
Dan Sabadis <dsabadis@sdl.com>
2021-02-22 21:34:14 +0200

    bagat beep alarma alerta de pret

--- samples/BinanceWpf/Binance.Wpf/MainWindow.xaml.cs
+++ samples/BinanceWpf/Binance.Wpf/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Threading;
+using System.Media;
 using System.Timers;
 using System.Windows;
 using Timer = System.Timers.Timer;
@@ -15,11 +16,14 @@ namespace Binance.Wpf
     {
         readonly BinanceApi binanceApi;
         readonly Timer _timer;
-        private SynchronizationContext _uiContext = SynchronizationContext.Current;
+        private decimal _targetPrice;
+        private decimal _currentPrice;
+        private bool _isPriceSearchAscending;
         public MainWindow()
         {
             InitializeComponent();
             binanceApi = new BinanceApi();
+            _targetPrice = -1;
 
             _timer = new Timer(2000) { Enabled = false, AutoReset = true };
             _timer.Elapsed += _timer_Elapsed;
@@ -29,7 +33,7 @@ namespace Binance.Wpf
         private async void _timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             string symbol = null;
-            Dispatcher.Invoke(() => symbol = FilteredComboBox1.Text);
+            Dispatcher.Invoke(() => symbol = AllPricesComboBox.Text);
 
             SymbolPrice price = null;
             try
@@ -49,6 +53,28 @@ namespace Binance.Wpf
                 }
             }
             Dispatcher.Invoke(() => PriceLabel.Content = $"{symbol}: {price.Value}");
+            Dispatcher.Invoke(() => TimeLabel.Content = $"{DateTime.Now.ToString("F", CultureInfo.InvariantCulture)}");
+
+            _currentPrice = price.Value;
+            if (_isPriceSearchAscending)
+            {
+                if (_targetPrice <= _currentPrice)
+                {
+                    SystemSounds.Beep.Play();
+                }
+            }
+            else
+            {
+                if (_targetPrice >= _currentPrice)
+                {
+                    SystemSounds.Exclamation.Play();
+                }
+            }
+
+            if (_targetPrice > 0)
+            {
+                _isPriceSearchAscending = _currentPrice < _targetPrice;
+            }
         }
 
         IEnumerable<SymbolPrice> _allPrices;
@@ -58,10 +84,10 @@ namespace Binance.Wpf
             {
                 _allPrices = await binanceApi.GetPricesAsync();
 
-                FilteredComboBox1.IsEditable = true;
-                FilteredComboBox1.IsTextSearchEnabled = false;
-                FilteredComboBox1.ItemsSource = _allPrices.Select(price => price.Symbol).ToList();
-                FilteredComboBox1.SelectedValue = "ETHBTC";
+                AllPricesComboBox.IsEditable = true;
+                AllPricesComboBox.IsTextSearchEnabled = false;
+                AllPricesComboBox.ItemsSource = _allPrices.Select(price => price.Symbol).ToList();
+                AllPricesComboBox.SelectedValue = "ETHBTC";
             }
 
             if ((string)PriceButton.Content == "Get Price")
@@ -75,6 +101,14 @@ namespace Binance.Wpf
                 _timer.Enabled = false;
             }
             // https://www.wpftutorial.net/GridLayout.html https://www.tutorialspoint.com/wpf/wpf_layouts.htm
+        }
+
+        private void AlertPriceTextBlock_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(!decimal.TryParse(AlertPriceTextBlock.Text, out _targetPrice))
+            {
+                _targetPrice = -1;
+            }
         }
     }
 }
